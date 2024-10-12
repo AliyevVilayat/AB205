b21ea01d73f27508b41304a7d2f50e3d01a12c94
AliyevVilayat <77es4pa@code.edu.az>
2024-10-12 17:15:08 +0400

    OOP Start project added

---
+++ AB205OOPStart/AB205OOPStart/Human.cs
@@ -0,0 +1,60 @@
+﻿namespace MyNameSpace2;
+
+
+#region Notes
+
+//Inheritance - Miras alma nəticəsində class adlandırılması dəyişir. Miras alan class DERIVED class adlanır. Miras verən class isə BASE class adlanır. 
+// DerivedClass : BaseClass
+
+//Miras alma prosesi nəticəsində nə baş verəcək?
+//Base class daxilində yer alan bütün field-lar, method-lar, constructor-lar bir sözlə desək Class Member-lar Derived class daxilində əl çatan olacaq.
+
+//This & Base keywords - Əgər ki, yer aldığımız class daxilində hər hansısa bir field-a müraciət etmək istəsək bunun üçün this keyword-dən istifadə etməliyik. Əgər ki, yer aldığımız class daxilində Base class-ın hər hansısa class member-a müraciət etmək istəsək bunun üçün Base keyword-dən istifadə edirik.
+
+
+//Constructor - Obyekt yaranan zaman( və ya obyekt new-lanan zaman və ya obyekt instance alınan zaman hər 3 ifadədə eyni mənanı kəsb edir.) işə düşən return type-ı olmayan bir method-dur. Class-ın adı ilə yazılır. Parametr qəbul edə bilər. Parameterless(default və ya boş da adlanır) constructor vardır.
+
+//Parameterless constructor, biz gəlib onu class daxilində yazmazsaq və ya heç bir overload halını yazmazsaq Assmebly səviyyəsində özü əlavə ediləcək. Əgər Parameterless constructor yazılmazsa və hər hansısa bir overload halı yazılarsa bu zaman parameterless halı işlənə bilməz.
+
+//Constructor calling - Constructor özünü call edə bilməz. Yalnızca rekursivlik yaranmadan digər constructor-ları çağıra bilərlər. Bu proses This və Base keyword ilə baş verir.
+
+//Constructor-lar bir-birini call edən zaman ilk olaraq call olunan işə düşür. Hər bir constructor yalnızca 1 constructor-u çağıra bilər.
+
+#endregion
+
+//[Access Modifier] [Class Keyword] [Class Name] - Class declaration-a bir nümunə
+public class BaseClass
+{
+
+    public BaseClass()
+    {
+
+    }
+    public BaseClass(string name)
+    {
+
+    }
+
+}
+
+
+public class DerivedClass : BaseClass
+{
+
+    public DerivedClass()
+    {
+        Console.WriteLine("Hello from Parameterless Constructor");
+
+    }
+    public DerivedClass(int num) //this() => DerivedClass()
+    {
+        Console.WriteLine("Hello from takes 1 Parameter Constructor");
+
+    }
+
+    public DerivedClass(int num, string name)
+    {
+        Console.WriteLine("Hello from takes 2 Parameter Constructor");
+    }
+
+}
---
+++ AB205OOPStart/AB205OOPStart/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Notes
+
+//OOP - Object Oriented Programming (Obyekt Yönümlü Proqramlaşdırma)
+//OOP-in prinsipləri:
+//Inheritance - Miras Alma
+//Polymorphism,
+//Encapsulation,
+//Abstraction
+
+//Class - Ram-da heç bir yer tutmur. Yalnız Class-dan alınan instance-lar yəni ondan törəyən obyektlər yer tutur.
+
+//Class member - Field, Constructor, Method
+
+//Field - Class daxilində təyin olunmuş və hər bir obyekt üçün xüsusi olan variable-lar yəni dəyişənlərdir.
+
+//Anonym obyektin(var obj = new{}) fied-larının dəyəri dəyişilməzdir. Yəni biz yalnızca object-i yaradan zaman dəyərləri təyin edə bilərik. 
+
+//Var keyword ilə declare olunmuş variable-ın tipi compile zamanı təyin edilir. Yəni yazılan dəyərə uyğun olaraq tipi götürür.Bu səbəbdən də unassign(heç bir dəyər mənimsədilmədən) şəkildə təyin oluna bilməzlər.
+
+//Dynamic keyword ilə declare olunmuş variable-ın tipi run zamanı təyin edilir. Bu səbəbdən istənilən tipdə dəyər mənimsədilə bilər.
+
+
+
+#endregion
+
+
