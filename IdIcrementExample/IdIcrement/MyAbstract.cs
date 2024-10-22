289533db2f801a8dd6ac79fffc1ab97e328d64a6
AliyevVilayat <77es4pa@code.edu.az>
2024-10-22 16:54:01 +0400

    Id Increment Example added

---
+++ IdIcrementExample/IdIcrement/MyAbstract.cs
@@ -0,0 +1,41 @@
+﻿namespace IdIncrement;
+
+#region Notes
+
+/*
+ Static Class member-lər bir başa olaraq Class-a aid olur. Class-a ilk müraciət edildiyi zaman gedir yaddaşda yerini ayırır.
+
+ Lakin Non-static Class member-lər isə bir başaolaraq obyekt-ə aid olur. Və hər bir obyekt üçün özünəxas olur.
+ */
+
+#endregion
+
+public abstract class MyAbstract
+{
+    private static int _counter;
+    public int Id { get; set; }
+
+    protected MyAbstract()
+    {
+        _counter++;
+        Id = _counter;
+    }
+
+}
+
+public class MyDerivedClass : MyAbstract
+{
+    public MyDerivedClass() : base()
+    {
+
+    }
+
+}
+
+public class MyDerivedClass2 : MyAbstract
+{
+    public MyDerivedClass2() : base()
+    {
+
+    }
+}
---
+++ IdIcrementExample/IdIcrement/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ 
+ 
+NOT: Id bizim tərəfimizdən daxil edilməməli, hər dəfə Employee'dan miras alan class'ın instace'ı alınan zaman avtomatik olaraq özü arxa tərəfdə Id-ə mənimsədilməlidir. Lakin bilməli olduğumuz məqam Id hər bir object üçün unique olmalıdır. Bu da deməkdir ki Id property readonly olmalıdır. Və static olaraq artan bir counter field'a ehtiyyacımız olacaq.
+ */
+
+
+using IdIncrement;
+
+MyDerivedClass obj = new MyDerivedClass();
+Console.WriteLine(obj.Id);
+
+MyDerivedClass obj2 = new MyDerivedClass();
+Console.WriteLine(obj2.Id);
+
+MyDerivedClass obj3 = new MyDerivedClass();
+Console.WriteLine(obj3.Id);
+
+Console.WriteLine("----------------------------------------");
+
+
+MyDerivedClass2 obj4 = new MyDerivedClass2();
+Console.WriteLine(obj4.Id);
+
+MyDerivedClass2 obj5 = new MyDerivedClass2();
+Console.WriteLine(obj5.Id);
+
+MyDerivedClass2 obj6 = new MyDerivedClass2();
+Console.WriteLine(obj6.Id);
